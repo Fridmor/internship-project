1015151ffea1604c3c5a624acac5cee4a0d3d37f
Dmitry Trufanov <dtflevir@gmail.com>
2022-03-16 12:19:37 +0300

    make it better

---
+++ src/main/java/com/github/fridmor/telegram/BotApp.java
@@ -0,0 +1,20 @@
+package com.github.fridmor.telegram;
+
+import com.github.fridmor.telegram.Bot;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+public class BotApp {
+    private static final String BOT_NAME = System.getenv("BOT_NAME");
+    private static final String BOT_TOKEN = System.getenv("BOT_TOKEN");
+
+    public static void main(String[] args) {
+        try {
+            TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
+            botsApi.registerBot(new Bot(BOT_NAME, BOT_TOKEN));
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+}
